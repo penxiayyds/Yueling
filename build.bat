d1297605b59081b93f0c72c3a9d37139a81a50ba
mcOnline <2137719630@qq.com>
2026-01-28 13:00:31 +0800

    格式化了一下代码

--- build.bat
+++ build.bat
@@ -9,15 +9,20 @@ echo 1. 构建后端服务器...
 cd /d "%~dp0server"
 call cargo build || (echo 后端构建失败 & exit /b 1)
 
+REM 补全前端环境
+echo.
+echo 2. 正在补全前端环境...
+cd /d "%~dp0Yueling"
+call npm install || (echo 补全失败 & exit /b 1)
+
 REM 构建前端
 echo.
-echo 2. 构建前端...
-cd /d "%~dp0Yueling"
+echo 3. 构建前端...
 call npm run build || (echo 前端构建失败 & exit /b 1)
 
 REM 构建 Tauri 应用
 echo.
-echo 3. 构建 Tauri 应用...
+echo 4. 构建 Tauri 应用...
 call cargo tauri build || (echo Tauri 构建失败 & exit /b 1)
 
 echo.
